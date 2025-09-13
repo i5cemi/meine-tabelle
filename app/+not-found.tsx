1af0a0f5db2a323884b8a5b8a90b6660b6b5d1d7
i5cemi <cercasov@web.de>
2025-09-13 23:49:00 +0200

    fix: redirect to home on not-found screen

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,21 +1,15 @@
-import { Link, Stack } from 'expo-router';
+import { useRouter } from 'expo-router';
 import { StyleSheet } from 'react-native';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+
+import { useEffect } from 'react';
 
 export default function NotFoundScreen() {
-  return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen does not exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
-    </>
-  );
+  const router = useRouter();
+  useEffect(() => {
+    router.replace('/');
+  }, [router]);
+  return null;
 }
 
 const styles = StyleSheet.create({
